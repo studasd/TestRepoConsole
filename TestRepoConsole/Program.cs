4fe8702f6602f1bb62e073c63e81a3dc5fa25136
fuser85 <fuser85@myoffice365.site>
2020-04-20 11:50:01 +0300

    Добавьте файлы проекта.

---
+++ TestRepoConsole/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestRepoConsole
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var MyApp = "Моя прила";
+		}
+	}
+}
